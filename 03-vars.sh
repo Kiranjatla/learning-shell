2b82b570b41e0ef26c01cec32dcdede8319ddc4c
KIRAN <ks@gmail.com>
2022-09-15 18:35:46 +0530

    Variables and Substitution

---
+++ 03-vars.sh
@@ -0,0 +1,13 @@
+a=10
+b=abc
+c=true
+
+echo value of a=$a
+
+# Command substitution
+DATE=$(date)
+echo today date is =$DATE
+
+# Arithmetic substitution
+ADD=$((2+3+4))
+echo Addition of 2+3+4 is $ADD
