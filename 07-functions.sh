c670489a7d9c8f3a9b9bd7f4858b65cf96b89b0e
KIRAN <ks@gmail.com>
2022-09-18 16:02:16 +0530

    sample function

--- 07-functions.sh
+++ 07-functions.sh
@@ -1,16 +1,23 @@
-##Declaration Way 1
+## Declaration Way 1
 function SAMPLE() {
-echo SAMPLE FUNCTION
+  echo a from main program = $a
+  a=100
+  echo a in function after override in function - $a
+  echo SAMPLE FUNCTION
+  b=10
+  echo first argument  to script = $1
 }
 
-
-
-
-
-##Declaration Way 2
+## Declaration Way 2
 SAMPLE1()
 {
   echo SAMPLE1 FUNCTION
 }
-SAMPLE
-SAMPLE1
+
+## Main Program
+a=10
+SAMPLE angel
+#SAMPLE1
+echo b from function = $b
+b=200
+echo b From function after override in main program- $b
