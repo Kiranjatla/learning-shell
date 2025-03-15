fd70f50baf767b9f5fe42c4e5fe8cea930630776
KIRAN <ks@gmail.com>
2025-03-15 17:15:28 +0530

    Arithmetic substitution

--- 03-vars.sh
+++ 03-vars.sh
@@ -8,5 +8,13 @@ echo value of b = $b
 
 echo value of c = $c
 
+
+#Command Substitution
 DATE=$(date)
 echo today date is $DATE
+
+#Arithmetic Substitution
+Add=$((2+3+4))
+
+addition of 2+3+4 = $ADD
+
